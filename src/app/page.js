7cd26ee202082d600306dbdd0d9e476489f96b69
unknown <internetmemes059@gmail.com>
2024-10-14 19:26:53 +0100

    first

--- src/app/page.js
+++ src/app/page.js
@@ -24,8 +24,32 @@ export default function Home() {
     "/8.jpeg",
     "/9.jpeg",
     "/10.jpeg",
-    "/11.jpeg",
+    "/12.jpeg",
+    "/13.jpeg",
+    "/14.jpeg",
+    "/15.jpeg",
+    "/16.jpeg",
+    "/17.jpeg",
+    "/18.jpeg",
+    "/19.jpeg",
+    "/20.jpeg",
+    "/21.jpeg",
+    "/22.jpeg",
+    "/23.jpeg",
+    "/24.jpeg",
+    "/25.jpeg",
+    "/26.jpeg",
+    "/27.jpeg",
+    "/28.jpeg",
+    "/29.jpeg",
+    "/30.jpeg",
+    "/31.jpeg",
+    "/32.jpeg",
+    "/33.jpeg",
+    "/34.jpeg",
+    "/35.jpeg"
   ];
+  
 
   const settings = {
     className: "center",
